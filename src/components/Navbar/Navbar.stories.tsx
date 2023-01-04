6ab4a123c09ff85734b243f9f9061e8688a72b37
NAGARRO\akshaysrivastava <akshay.srivastava@nagarro.com>
2023-01-04 11:38:34 +0530

    add navbar store provider in storybook

--- src/components/Navbar/Navbar.stories.tsx
+++ src/components/Navbar/Navbar.stories.tsx
@@ -1,16 +1,25 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Provider } from 'react-redux'
+import { store } from '../../features/store'
 import Navbar from './Navbar'
 
 export default {
   title: 'Navbar',
   component: Navbar,
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
 } as ComponentMeta<typeof Navbar>
 
 const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />
 
 export const Primary = Template.bind({})
 
-export const Success = Template.bind({})
+// export const Success = Template.bind({})
 
 Primary.args = {
   // primary: true,
@@ -18,8 +27,8 @@ Primary.args = {
   color: 'Primary',
 }
 
-Success.args = {
-  // Success: true,
-  // label: 'Navbar-Success',
-  color: 'Success',
-}
+// Success.args = {
+//   // Success: true,
+//   // label: 'Navbar-Success',
+//   color: 'Success',
+// }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 // import { ReactNode } from 'react'
 
 import { Link } from 'react-router-dom'
-// import { useAppSelector } from '../../hooks/hooks'
+import { useAppSelector } from '../../hooks/hooks'
 
 import './Navbar.scss'
 
@@ -11,7 +11,7 @@ import './Navbar.scss'
 // }
 
 const Navbar = (props: { color: string }) => {
-  // const items = useAppSelector((state) => state.cart)
+  const items = useAppSelector((state) => state.cart)
   const { color } = props
   return (
     <div className={`navBar${color}`}>
@@ -23,11 +23,7 @@ const Navbar = (props: { color: string }) => {
         <Link className="navLink" to="/cart">
           Cart
         </Link>
-        <span className="cartCount">
-          Cart Items:0
-          {/* {
-           items.length} */}
-        </span>
+        <span className="cartCount">Cart Items : {items.length}</span>
       </div>
     </div>
   )
